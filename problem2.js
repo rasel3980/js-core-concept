855e3a8f8b154e538a586cc9c337f06a006c520f
rasel3980 <rkrasel3980@gmail.com>
2025-06-27 17:37:22 +0600

    problem2 solved

---
+++ problem2.js
@@ -0,0 +1,10 @@
+// Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+
+var mathematics = 70;
+var biology = 75;
+var chemistry = 80;
+var physics = 69;
+var bangla = 80;
+var totalMark = mathematics + biology + chemistry + physics + bangla;
+var average = totalMark / 5;
+console.log("Average Mark " + average);
